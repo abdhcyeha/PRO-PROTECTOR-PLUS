6711c7527545b0259d66275b3929fe8b3812ae03
FartLord <96386481+abdhcyeha@users.noreply.github.com>
2022-04-18 01:48:46 +0200

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,22 @@
+import pyautogui
+import time
+import requests
+import termcolor
+import socks
+import urllib
+import json
+
+time.sleep(3)
+print("ANTICHEAT LOADING---- MADE BY FARTLORD AND CYPPHI")
+
+PLAYER_HACKING = input("Which Player is Hacking?")
+def Main():
+   while AntiCheat = True:
+   pyautogui.keyDown("t")
+   pyautogui.keyUp("t")
+   pyautogui.typewrite("/ban "+PLAYER_HACKING)
+   pyautogui.keyDown("enter")
+   pyautogui.keyUp("enter")
+
+time.sleep(2)
+Main()
